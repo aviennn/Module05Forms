8432e9687bd758a5a778537c66277051f351d873
Avien <avienflairebatul@gmail.com>
2024-05-03 00:15:45 +0800

    Add project files.

---
+++ Module05Forms/Module05Forms/PersonalInfoPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Module05Forms
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PersonalInfoPage : ContentPage
+    {
+        public PersonalInfoPage()
+        {
+            InitializeComponent();
+        }
+       
+        void OnDateSelected(object sender, DataErrorsChangedEventArgs args)
+        {
+            DateTime birthdate = birthDatePicker.Date;
+            DateTime today = DateTime.Today;
+            int age = today.Year - birthdate.Year;
+
+            ageComputation.Text = $"Age: {age}";
+        }
+        
+        void OnSubmitClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Success", "You have submitted the form", "OK");
+        }
+    }
+}
